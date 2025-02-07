4f59f721982e70a88efae281c1505aacb6e62e0c
TylerBurge <burgetyl@gmail.com>
2025-02-07 12:22:47 -0600

    Added word list

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const word = "brave"
+var word = "brave";
+var wordList = new Set();
 let attempts = 1;
 
 function submit() {
     let guess = document.getElementById("guess").value.toLowerCase();
     document.getElementById("guess").value = "";
-    if (!isValidGuess(guess)) {
+    if (!wordList.has(guess)) {
         alert("Invalid Guess");
     } else {
         for (let i = 0; i < 5; i++) {
-            const tile = document.getElementById("tile"+attempts+"-"+i);
+            const tile = document.getElementById("tile" + attempts + "-" + i);
             tile.innerHTML = guess[i].toUpperCase();
             if (guess[i] === word[i]) {
                 tile.classList.add("correct");
@@ -25,6 +26,13 @@ function submit() {
     }
 }
 
-function isValidGuess(guess) {
-    return /^[A-Za-z]{5}$/.test(guess);
-}
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("sorted_words.txt")
+        .then(response => response.text())
+        .then(text => {
+            text.split(/\r?\n/).forEach(word => wordList.add(word.trim()));
+            //console.log(wordList);
+            word = [...wordList][Math.floor(Math.random() * wordList.size)];
+            console.log(word);
+        });
+});
